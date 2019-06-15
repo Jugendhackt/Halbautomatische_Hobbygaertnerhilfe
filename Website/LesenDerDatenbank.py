c2ca6fb49691b62f8c7a35d229294f3acfd50bc3
Ncc3000 <51849580+Ncc3000@users.noreply.github.com>
2019-06-15 15:40:30 +0200

    Verbesserung des Uplouds

--- Website/LesenDerDatenbank.py
+++ Website/LesenDerDatenbank.py
@@ -1,32 +1,23 @@
 import time
-                                            # Das ist das lesen der Datei
-Temperatur = open("Temperatur.txt", "r")
-temp = Temperatur.readline()
-Temperatur.close()
-temp2 = open("Temperatur.txt").read()
-Luftdruck = open("Luftdruck.txt", "r")
-luftdr = Luftdruck.readline()
-Luftdruck.close()
-Luftfeuchtigkeit = open("Luftfeuchtigkeit.txt", "r")
-luftfe = Luftfeuchtigkeit.readline()
-Luftfeuchtigkeit.close()
-Bodenfeuchtigkeit = open("Bodenfeuchtigkeit.txt", "r")
-boden = Bodenfeuchtigkeit.readline()
-Bodenfeuchtigkeit.close()
-Helligkeit = open("Helligkeit.txt", "r")
-helli = Helligkeit.readline()
-Helligkeit.close()
-
-
-
-
-
-                                      #   temp, luftdr, luftfe, boden, helli
+#   temp, luftdr, luftfe, boden, helli
 
 while True:
+    Temperatur = open("Temperatur.txt", "r")
+    temp = Temperatur.readline()
+    Temperatur.close()
+    Luftdruck = open("Luftdruck.txt", "r")
+    luftdr = Luftdruck.readline()
+    Luftdruck.close()
+    Luftfeuchtigkeit = open("Luftfeuchtigkeit.txt", "r")
+    luftfe = Luftfeuchtigkeit.readline()
+    Luftfeuchtigkeit.close()
+    Bodenfeuchtigkeit = open("Bodenfeuchtigkeit.txt", "r")
+    boden = Bodenfeuchtigkeit.readline()
+    Bodenfeuchtigkeit.close()
+    Helligkeit = open("Helligkeit.txt", "r")
+    helli = Helligkeit.readline()
+    Helligkeit.close()
     web = open("Server.html").read()
-    #a = web.read()
-    #web.close()
     web = web.replace("[[temp]]", temp)
     web = web.replace("[[luftdr]]", luftdr)
     web = web.replace("[[luftfe]]", luftfe)
