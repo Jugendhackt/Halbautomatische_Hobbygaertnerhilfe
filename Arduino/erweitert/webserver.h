5deb819aa4061b9841d85b3d3de63902b32e1467
Gucky <51849622+RaphaelKlug@users.noreply.github.com>
2021-07-17 09:12:09 +0200

    Create webserver.h

---
+++ Arduino/erweitert/webserver.h
@@ -0,0 +1,11 @@
+void web_init(){
+  server.on("/", handle_OnConnect);
+  server.onNotFound(handle_NotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void handle_OnConnect(){}
+
+void handle_NotFound(){}
