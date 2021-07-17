98a624bccf24b7da4717b2d524b4920ec3a91267
Gucky <51849622+RaphaelKlug@users.noreply.github.com>
2021-07-17 15:32:45 +0200

    Update gpio.h

--- Arduino/erweitert/gpio.h
+++ Arduino/erweitert/gpio.h
@@ -1,3 +1,12 @@
+#include <servo.h>
+int MUXPinS0 = D7;
+int MUXPinS1 = D6;
+int MUXPinS2 = D5;
+int MUXPinS3 = D4;
+
 void gpio_init(){
-  //lot of stuff
+  pinMode(MUXPinS0, OUTPUT);
+  pinMode(MUXPinS1, OUTPUT);
+  pinMode(MUXPinS2, OUTPUT);
+  pinMode(MUXPinS3, OUTPUT);
 }
