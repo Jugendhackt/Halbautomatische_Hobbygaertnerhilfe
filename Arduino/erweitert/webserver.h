166aabb557c2cbeddce57c6ad0dc98054bf534f6
Gucky <51849622+RaphaelKlug@users.noreply.github.com>
2021-07-17 09:51:40 +0200

    Update webserver.h

--- Arduino/erweitert/webserver.h
+++ Arduino/erweitert/webserver.h
@@ -1,12 +1,16 @@
 #include <ESP8266WebServer.h>
+
 void web_init(){
   server.on("/", handle_OnConnect);
   server.onNotFound(handle_NotFound);
 
   server.begin();
-  Serial.println("HTTP server started");
 }
 
-void handle_OnConnect(){}
+void web_loop(){server.handleClient();}
+
+void handle_OnConnect(){
+
+}
 
 void handle_NotFound(){}
