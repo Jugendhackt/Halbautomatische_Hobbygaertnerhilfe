861ba56ab51c0533195194c44bd9244172e0dfa4
Gucky <51849622+RaphaelKlug@users.noreply.github.com>
2021-07-29 17:45:17 +0200

    Update webserver.h

--- Arduino/erweitert/webserver.h
+++ Arduino/erweitert/webserver.h
@@ -5,11 +5,23 @@ ESP8266WebServer server(80);
 void web_init(){
   server.on("/", handle_OnConnect);
   server.onNotFound(handle_NotFound);
-
+  server.on("/clock",c);
+  server.on("/setclock",HTTP_PUT,sc);
   server.begin();
 }
+int clock;
+void c(){
+  server.send(200,"text/html",String(clock))
+}
 
-void web_loop(){server.handleClient();}
+void sc(){
+  String temp=server.arg("clock");
+  int i
+  for(i=0;i<temp.length;i++){if(!isDigit(temp.charAt(i))return;}
+  clock=toInt(temp);
+}
+
+void web_loop(){server.handleClient();clock++;}
 
 void handle_OnConnect(){
   html=sd_read('/index.html')||'<html></html>';
