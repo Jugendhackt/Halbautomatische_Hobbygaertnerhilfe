582350fadf2db6900cd984a933b35017e77269be
Gucky <51849622+RaphaelKlug@users.noreply.github.com>
2021-11-11 18:07:52 +0100

    Update Arduino.ino
    Detect default fails

--- Arduino/erweitert/Arduino.ino
+++ Arduino/erweitert/Arduino.ino
@@ -4,24 +4,47 @@
 #include "config.h"
 #include "gpio.h"
 
+bool error=false;
+
+#ifndef ssid
+error=true;
+#endif
+
+#ifndef password
+error=true;
+#endif
 
 void setup(){
-  Serial.println("Connecting to ");
-  Serial.println(ssid);
+  Serial.begin(9600);
+  if(error==false){
+    Serial.println("Connecting to ");
+    Serial.println(ssid);
 
-  //connect to your local wi-fi network
-  WiFi.begin(ssid, password);
+    //connect to your local wi-fi network
+    WiFi.begin(ssid, password);
 
-  //check wi-fi is connected to wi-fi network
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print(".");
+    //check wi-fi is connected to wi-fi network
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(1000);
+      Serial.print(".");
+    }
+    sd_init();
+    gpio_init();
+    web_init();
+  }else{
+    error_init();
+    pinMode(13,OUTPUT)
+    Serial.println("Fail with default data");
   }
-sd_init();
-gpio_init();
-web_init();
 }
 
 void loop(){
-web_loop();
+  if(error==false){
+    web_loop();
+  }else{
+    digitalWrite(13, HIGH);
+    delay(1000);
+    digitalWrite(13, LOW);
+    delay(1000);
+  }
 }
