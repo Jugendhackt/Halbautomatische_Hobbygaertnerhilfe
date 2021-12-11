e277b1eb18b592db24755e6966f1e1738a3980cc
Gucky <51849622+RaphaelKlug@users.noreply.github.com>
2021-12-11 16:25:23 +0100

    Update webserver.h

--- Arduino/erweitert/webserver.h
+++ Arduino/erweitert/webserver.h
@@ -1,20 +1,45 @@
-#include <ESP8266WebServer.h>
+//#include <ESP8266WebServer.h>
+#include <ESPAsyncWebServer.h>
 
-ESP8266WebServer server(80);
+//ESP8266WebServer server(80);
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
 
 void web_init(){
-  server.on("/", handle_OnConnect);
-  server.onNotFound(handle_NotFound);
-  server.on("/clock",c);
-  server.on("/setclock",HTTP_PUT,sc);
+  server.serveStatic("/", SPIFFS, "/server/").setDefaultFile("index.html");
+  server.onNotFound([](AsyncWebServerRequest *request){
+    request->send(404);
+  });
+  ws.onEvent(onWsEvent);
+  server.addHandler(&ws);
   server.begin();
 }
 int clock;
-void c(){
-  server.send(200,"text/html",String(clock))
+
+void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len){
+ 
+  if(type == WS_EVT_CONNECT){
+ 
+    Serial.println("Websocket client connection received");
+    client->text("clock:"+String(clock));
+ 
+  } else if(type == WS_EVT_DISCONNECT){
+    Serial.println("Client disconnected");
+ 
+  } else if(type == WS_EVT_ERROR){
+    Serial.print("ws");
+    Serial.print(server->url());
+    erial.print("error", client->id(),": ", *((uint16_t*)arg), (char*)data));
+  } else if(type==WS_EVT_DATA){
+    
+  }
 }
 
-void sc(){
+void c(){
+  request->send(200,"text/html",String(clock))
+}
+
+void sc(data){
   String temp=server.arg("clock");
   int i
   for(i=0;i<temp.length;i++){if(!isDigit(temp.charAt(i))return;}
@@ -22,13 +47,3 @@ void sc(){
 }
 
 void web_loop(){server.handleClient();clock++;}
-
-void handle_OnConnect(){
-  html=sd_read('/index.html')||'<html></html>';
-  server.send(200, "text/html", html);
-}
-
-void handle_NotFound() {
-  server.send(404, "text/plain", "Not found");
-}
-
